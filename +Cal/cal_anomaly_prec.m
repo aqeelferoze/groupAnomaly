22f04cfe4852c92c092b81879fe439c3d2aa57c3
Rose Yu <yuqi.rose@hotmail.com>
2014-02-11 12:12:49 -0800

    Update synthetic script

--- +Cal/cal_anomaly_prec.m
+++ +Cal/cal_anomaly_prec.m
@@ -5,7 +5,7 @@ function [ prec ] = cal_anomaly_prec( bad_idx, scores, thres )
 num_total = length(scores);
 num_bads = length(bad_idx);
 [scores_sort, order ]  =  sort(scores, 1, 'descend');
-num_retrvl = floor(num_total* thres);
+num_retrvl = ceil(num_total* thres);
 anomaly_idx  = order(1:num_retrvl) ;
 prec = length(intersect(bad_idx, anomaly_idx))/num_bads;
 end
